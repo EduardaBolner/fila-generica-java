2cf1b7bd2c1aee6bd363cafcfa91c60370e083ab
Eduarda de Q. F. Bolner <https://github.com/EduardaBolner/Brincando-de-HTML.git>
2026-06-01 21:48:34 -0300

    feat: adiciona classe Cliente

---
+++ scr/Cliente.java
@@ -0,0 +1,22 @@
+package scr;
+
+public class Cliente {
+  
+}
+public class Cliente {
+    private String nome;
+    private String cpf;
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() { return nome; }
+    public String getCpf()  { return cpf; }
+
+    @Override
+    public String toString() {
+        return "Cliente { nome='" + nome + "', cpf='" + cpf + "' }";
+    }
+}
